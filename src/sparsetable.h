2a8ca01c7d1e567c2aa3f32452bc46fd057d1bce
Patrick Perry <patperry@gmail.com>
2011-09-27 14:56:33 -0400

    Add LICENSE, README

--- src/sparsetable.h
+++ src/sparsetable.h
@@ -1,5 +1,37 @@
-#ifndef _SPARSETABLE_H
-#define _SPARSETABLE_H
+#ifndef SPARSETABLE_H
+#define SPARSETABLE_H
+
+/* Copyright (c) 2011, Patrick O. Perry
+ * Copyright (c) 2005, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following disclaimer
+ *       in the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the names of Patrick O. Perry, Google Inc,. nor the names of
+ *       their contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 /* An intmap maps the integers 0..n-1 to values.  The implementation on
  * Google's "sparsetable".
@@ -69,4 +101,4 @@ struct sparsetable_iter sparsetable_iter_make(const struct sparsetable *t);
 void sparsetable_iter_reset(struct sparsetable_iter *it);
 bool sparsetable_iter_advance(struct sparsetable_iter *it);
 
-#endif /* _SPARSETABLE_H */
+#endif /* SPARSETABLE_H */
